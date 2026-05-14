6febf2bd1b6dfeda41a1d4305b56fd050cdf3140
Daedaleus <github@pkeil.de>
2026-05-14 23:13:23 +0200

    style(backend): 🎨 apply cargo fmt

--- backend/src/service/group.rs
+++ backend/src/service/group.rs
@@ -152,7 +152,9 @@ impl GroupService {
                 ));
             }
             if u.len() > 200 {
-                return Err(AppError::Validation("Link zu lang (max. 200 Zeichen)".into()));
+                return Err(AppError::Validation(
+                    "Link zu lang (max. 200 Zeichen)".into(),
+                ));
             }
         }
 
@@ -313,7 +315,11 @@ mod tests {
         async fn find_all(&self) -> Result<Vec<Group>, surrealdb::Error> {
             panic!()
         }
-        async fn set_discord_invite(&self, _: String, _: Option<String>) -> Result<(), surrealdb::Error> {
+        async fn set_discord_invite(
+            &self,
+            _: String,
+            _: Option<String>,
+        ) -> Result<(), surrealdb::Error> {
             panic!()
         }
     }
--- backend/src/service/user.rs
+++ backend/src/service/user.rs
@@ -246,7 +246,11 @@ mod tests {
         async fn find_all(&self) -> Result<Vec<Group>, surrealdb::Error> {
             panic!()
         }
-        async fn set_discord_invite(&self, _: String, _: Option<String>) -> Result<(), surrealdb::Error> {
+        async fn set_discord_invite(
+            &self,
+            _: String,
+            _: Option<String>,
+        ) -> Result<(), surrealdb::Error> {
             panic!()
         }
     }
